5b00e8d3d1a6ada82e970877c1a0007079719aee
Emanuel Rylke <ema-fox@web.de>
2013-06-04 12:20:18 +0000

    stuff

--- src/ankichallenge/core.clj
+++ src/ankichallenge/core.clj
@@ -2,27 +2,57 @@
   (:use [noir.server :only [start]]
         [clojure.data.json :only [write-str]]
         noir.core
-        hiccup.core))
+        hiccup.core)
+  (:import [java.io FileNotFoundException]))
 
-(defonce data (ref {:points {}
-                    :init-points {}}))
+(defn write-to [r path]
+  (let [a (agent false)]
+    (add-watch r path
+               (fn [_ _ _ _]
+                 (send a (fn [pending]
+                           (when-not pending
+                             (.start (Thread. (fn []
+                                                (Thread/sleep (* 10 60 1000))
+                                                (send-off a (fn [_]
+                                                              (spit path @r)
+                                                              false)))))
+                             true))))))
+  r)
+
+(defn persref-fn [path initf]
+  (write-to (ref (try
+                   (read-string (slurp path))
+                   (catch FileNotFoundException e
+                     (initf))))
+            path))
+
+(defmacro persref [path init]
+  `(persref-fn ~path (fn [] ~init)))
+
+(defonce data (persref "data.clj"
+                       {:points {}
+                        :init-points {}}))
+
+(defn relps [{:keys [points init-points]}]
+  (merge-with - points init-points))
 
 (defn show-points []
   (str (:points @data)))
 
+(defn update-points [name amount]
+  (dosync
+   (alter data update-in [:init-points name] #(or % amount))
+   (alter data assoc-in [:points name] amount)))
+
 (defpage [:post "/points"] {:keys [name amount]}
-  (let [amount (Integer. amount)]
-    (dosync
-     (alter data update-in [:init-points name] #(or % amount))
-     (alter data assoc-in [:points name] amount))
-    (write-str {:msg (show-points)})))
+  (update-points name (Integer. amount))
+  (write-str {:msg (show-points)}))
 
 (defpage "/" []
   (html
    [:h1 "Ankichallenge high score"]
    [:div (for [[name points]
-               (sort-by second >
-                        (merge-with - (:points @data) (:init-points @data)))]
+               (sort-by second > (relps @data))]
            [:div (str name ": " points)])]))
 
 (defn -main [port]
